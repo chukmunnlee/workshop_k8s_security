0aff3256a03cefa4021f2d2438de1b5e3218ae87
Chuk <chukmunnlee@gmail.com>
2021-03-09 15:17:37 +0800

    fortune image

---
+++ 01_execution_context/main.js
@@ -0,0 +1,42 @@
+const FORTUNE_COOKIES = require('fortune-cookie')
+const morgan = require('morgan')
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const PORT = parseInt(process.env.PORT) || 3000
+
+const app = express()
+
+const since = Math.floor((new Date()).getTime() / 1000)
+
+app.engine('hbs', hbs({ defaultLayout: 'default.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(morgan('combined'))
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/healthz', (req,resp) => {
+	resp.status(200).type('application/json')
+		.json({
+			status: 'OK',
+			duration: Math.floor(((new Date()).getTime() / 1000) - since),
+			since,
+		})
+})
+
+app.use((req, resp) => {
+	const text = rndCookie(FORTUNE_COOKIES)
+	resp.status(200).type('text/html')
+		.render('index', { text })
+})
+
+app.listen(PORT, () => {
+	console.info(`Application started on port ${PORT} at ${new Date()}`)
+})
+
+// random cookie
+const rndCookie = (cookies) => {
+	const idx = Math.floor(Math.random() * cookies.length)
+	return cookies[idx]
+}
